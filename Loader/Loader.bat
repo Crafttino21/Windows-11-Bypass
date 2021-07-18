1f75e4c5c2e4c0b10fe498b9d21a5606e3978bc4
CraftModzZ Crafttino21 <58913870+Crafttino21@users.noreply.github.com>
2021-07-18 19:12:15 +0200

    The main files of the First version
    The first version of the Bypass. The "Loader.bat" is the sorce code for the launcher and "bypass.reg" is the bypass

---
+++ Loader/Loader.bat
@@ -0,0 +1,90 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcAqLMmG7OpEZ++Pv4Pq7tUQTBbN/WpbL26edJfMvyFz0dIU+02lmmtIcDQhLMACuaW8=
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBVdQg6DAE+/Fb4I5/jH+OOJ8xxddvsveZzI36eyDO8S7kD2SaEk2CsLq8IBHBddXAu7bxwn52taswQ=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Win11 Bypasses
+color 6
+cls
+echo IMPORTANT WARNING!:
+echo Bypassing the minimum requirements for windows 11 can cause serious errors in software and hardware!
+echo Use that is at your own risk! I am not responsible for any damage!
+timeout /T 10 /nobreak
+color a
+cls
+:ans
+title Windows 11 Instalation Bypass by CraftmodzZ
+cls
+echo.
+echo IMPORTANT WARNING!:
+echo Bypassing the minimum requirements for windows 11 can cause serious errors in software and hardware!
+echo Use that is at your own risk! I am not responsible for any damage!
+echo ===========
+echo.
+color a
+echo [1]Load Bypass
+echo [2]Inspect bypass Sorce
+echo [x]close 
+echo.
+
+set asw=0
+set /p asw="Choose what you answare: "
+
+if %asw%==1 goto bypass
+if %asw%==2 goto Sorce
+if %asw%==x goto exit
+goto ans
+
+:bypass
+echo Load Sorce and inject
+regedit /s bypass.reg
+goto rs
+
+:rs
+title Succsesfully!
+echo Bypass Loaded!
+timeout /T 8 /nobreak
+goto ans
+
+:Sorce
+
+
+:exit
+title Good Bye:)
+echo Good by and i hope i can help You :)
+echo .
+echo .
+echo YouTube: Crafttino21 der Aller Echte
+echo TT: Pain_the_Real
+echo credits: Bens_Hardware
+timeout /T 5 
